164efeb0a2c65f64485c293285ae0637d0bc896f
Andrecuogh <128002890+Andrecuogh@users.noreply.github.com>
2023-12-29 18:57:25 +0100

    Add files via upload

--- schedina/scripts/display/window_config.py
+++ schedina/scripts/display/window_config.py
@@ -1,3 +1,16 @@
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy import utils
+from kivy.core.text import LabelBase, DEFAULT_FONT
+
+colors = {
+    'label': utils.get_color_from_hex('#23395d'),
+    'text': utils.get_color_from_hex('#ffffff'),
+    'background': utils.get_color_from_hex('#afeeee')
+}
+
+LabelBase.register(DEFAULT_FONT, "display/fonts/calibri/calibri.ttf")
+
 def applayout(self):      
 
     self.gfgshead = GridLayout(size_hint=(0.8, 0.05), pos_hint={'center_x': .55, 'center_y': .95}, cols=8)
@@ -18,10 +31,10 @@ def applayout(self):
 
 
     self.teamh = Button(size_hint=(0.1, 0.09), pos_hint={'center_x': .08, 'center_y': .87},
-                            font_size = 40, color = 'yellow', background_color = 'darkgreen')
+                            font_size = 40, color = colors['text'], background_color = colors['label'])
 
     self.teama = Button(size_hint=(0.1, 0.09), pos_hint={'center_x': .08, 'center_y': .77},
-                            font_size = 40, color = 'yellow', background_color = 'darkgreen')
+                            font_size = 40, color = colors['text'], background_color = colors['label'])
 
 
     self.ox2head = GridLayout(size_hint=(0.5, 0.05), pos_hint={'center_x': .5, 'center_y': .40}, cols=3)
@@ -30,12 +43,12 @@ def applayout(self):
 
 
     self.prec = Button(size_hint=(0.3, 0.1), pos_hint={'center_x': .25, 'center_y': .1},
-                              text='Precedente', font_size = 60, color = 'yellow', background_color = 'darkgreen')
+                              text='Precedente', font_size = 60, color = colors['text'], background_color = colors['label'])
 
     self.suc = Button(size_hint=(0.3, 0.1), pos_hint={'center_x': .75, 'center_y': .1},
-                              text='Successivo', font_size = 60, color = 'yellow', background_color = 'darkgreen')
+                              text='Successivo', font_size = 60, color = colors['text'], background_color = colors['label'])
 
-    self.gfgsdesc = Button(text='GOL', font_size = 50, color = 'yellow', size_hint=(0.1, 0.05),
-                      pos_hint={'center_x': .08, 'center_y': .95}, background_color = 'darkgreen')
+    self.gfgsdesc = Button(text='GOL', font_size = 50, color = colors['text'], size_hint=(0.1, 0.05),
+                      pos_hint={'center_x': .08, 'center_y': .95}, background_color = colors['label'])
 
     self.partita = 0
--- schedina/scripts/display/window_obj.py
+++ schedina/scripts/display/window_obj.py
@@ -1,10 +1,12 @@
 from kivy.uix.button import Button
+from display.window_config import colors
+import numpy as np
 
 def new_button(text):
     button = Button(text = str(text), 
                     font_size = 50, 
-                    color = 'yellow', 
-                    background_color = 'darkgreen')
+                    color = colors['text'], 
+                    background_color = colors['label'])
     return button
 
 def proba_string(df, match, index):
